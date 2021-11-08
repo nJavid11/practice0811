79519d68f80d52f34522a72997de80a252c3ddfc
nJavid11 <tu201803071@code.edu.az>
2021-11-08 13:22:19 +0400

    Practice

---
+++ ValueAddition/ValueAddition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ValueAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[1];
+            
+            AddValue(arr);
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void AddValue(int[] arr)
+        {
+            try
+            {
+                Console.Write("Ededi daxil edin: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                arr[0] = num;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+
+}
